9a98134d217c17a9f2dc6bea22681947695fd6ed
Leah <LeahKahwadjian@gmail.com>
2020-06-07 14:20:53 -0400

    sharing repo

---
+++ color.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch");
+
+function getHex(hex) {
+  fetch(`http://thecolorapi.com/id?hex=${hex}`)
+    .then((res) => res.json())
+    .then((res) => console.log("first one "));
+}
+
+// function getHex(hex) {
+//   fetch(`http://reddit.com`)
+//     .then((res) => res.text())
+//     .then((res) => console.log("workeddddd?"));
+// }
+
+getHex(443356);
